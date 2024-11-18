37117ca9ac512826129e64bbb45e62f6e0243cbb
adithiachristian7 <adithiachristian@gmail.com>
2024-11-18 12:41:31 +0800

    menambah firebase componen homepage/download

--- src/components/homePage/Download.jsx
+++ src/components/homePage/Download.jsx
@@ -1,27 +1,32 @@
-const Img1 = "./images/download/appstore.svg";
-const Img2 = "./images/download/googleapp.svg";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { useEffect, useState } from "react";
 
-function Download() {
+const Download = () => {
+  const [download, setDownload] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const downloadRef = ref(db, "homepage/download");
+    onValue(downloadRef, (snapshot) => {
+      const data = snapshot.val();
+      setDownload(data);
+    });
+  }, []);
   return (
     <>
       <section className="download-section">
         <div className="container">
           <div className="download-text">
-            <h2>Download our app to get most out of it</h2>
-            <p>
-              Thrown shy denote ten ladies though ask saw. Or by to he going
-              think order event music. Incommode so intention defective at
-              convinced. Led income months itself and houses you.
-            </p>
+            <h2>{download.judul}</h2>
+            <p>{download.text}</p>
             <div className="download-text__btns">
-              <img alt="download_img" src={Img2} />
-              <img alt="download_img" src={Img1} />
+              <img alt="download_img" src={download.gambar1} />
+              <img alt="download_img" src={download.gambar2} />
             </div>
           </div>
         </div>
       </section>
     </>
   );
-}
+};
 
 export default Download;
